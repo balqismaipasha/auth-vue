cd38d9b347074d3535260d055a4f27bcac27e7b3
balqismaipasha <balqismaipasha@gmail.com>
2022-07-27 08:40:11 +0700

    commit

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    Name: <input type="text" v-model="name"/> <br>
+    Email: <input type="text" v-model="email"/> <br>
+    Password: <input type="text" v-model="password"/> <br>
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'LoginForm',
+  data(){
+    return{
+      name: '',
+      email:'',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      console.log(this.name);
+      console.log(this.email);
+      console.log(this.password);
+    }
+  }
+}
+</script>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    Name: <input type="text" v-model="name"/> <br>
+    Email: <input type="text" v-model="email"/> <br>
+    Password: <input type="password" v-model="password"/> <br>
+  <button @click="signup">SignUp</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name:'SignupForm',
+  data(){
+    return{
+      name: '',
+      email:'',
+      password: ''
+    }
+  },
+  methods: {
+    signup() {
+      let newUser = {
+        name: this.name,
+        email:this.email,
+        password: this.password
+      }
+      // console.log(newUser);
+
+      axios.post('http://localhost:5000/signup', newUser);
+
+    }
+  }
+}
+</script>
